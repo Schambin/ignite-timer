5fb9a325c853590879f550849c5af576264fb0a4
Kauan <raidoka546@gmail.com>
2024-07-31 14:59:45 -0400

    ignite timer starteed

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Button } from "./components/Button";
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/default";
+
+export function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <h1>oi manaaa</h1>
+      <Button variant="primary" />
+      <Button variant="secondary"/>
+      <Button variant="danger"/>
+      <Button variant="success"/>
+      <Button />
+    </ThemeProvider>
+  )
+}
